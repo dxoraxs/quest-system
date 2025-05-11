018c92aa55d70c2b33da68cad21ecde30f3abe48
Valentin Nazarenko <dxoraxslife@gmail.com>
2025-05-11 15:05:10 +0400

    Added ObjectSpawnData

---
+++ Assets/Scripts/Data/KillableObjectSpawnConfig.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace QuestSystem.Data
+{
+    [CreateAssetMenu(fileName = "lillable_object_spawn_config",
+        menuName = "Configs/" + nameof(KillableObjectSpawnConfig), order = 0)]
+    public class KillableObjectSpawnConfig : ScriptableObject
+    {
+        [field: SerializeField] public KillableObjectSpawnData[] Spawns { get; private set; }
+        [field: SerializeField] public Vector3 AreaSize { get; private set; } = new Vector3(10, 0, 10);
+    }
+}
---
+++ Assets/Scripts/Data/KillableObjectSpawnData.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace QuestSystem.Data
+{
+    [System.Serializable]
+    public class KillableObjectSpawnData
+    {
+        [field: SerializeField] public ObjectType Type { get; private set; }
+        [field: SerializeField] public int Count { get; private set; }
+    }
+}
